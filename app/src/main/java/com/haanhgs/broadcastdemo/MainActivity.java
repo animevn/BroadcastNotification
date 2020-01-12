e93eb68da38c7430888be7605b65cc27239959d2
animevn <haanhth21@gmail.com>
2020-01-12 10:30:17 +0700

    ok

--- app/src/main/java/com/haanhgs/broadcastdemo/MainActivity.java
+++ app/src/main/java/com/haanhgs/broadcastdemo/MainActivity.java
@@ -1,15 +1,20 @@
 package com.haanhgs.broadcastdemo;
 
+import android.app.NotificationManager;
+import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BroadcastObserver{
 
     @BindView(R.id.bnNotify)
     Button bnNotify;
@@ -20,25 +25,82 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.bnCancel)
     Button bnCancel;
 
+    private Broadcast receiver;
+    private static final int ID = 220479;
+    private Notification notification;
+    private NotificationManager manager;
+
+    private void initReceiver(){
+        receiver = new Broadcast();
+        registerReceiver(receiver, new IntentFilter(Notification.REMOTE_ACTION));
+    }
+
+    private void initNotification(){
+        manager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+        notification = new Notification(this);
+    }
+
+    private void setButtonState(boolean notify, boolean update, boolean cancel){
+        bnNotify.setEnabled(notify);
+        bnUpdate.setEnabled(update);
+        bnCancel.setEnabled(cancel);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
-
+        initReceiver();
+        initNotification();
+        setButtonState(true, false, false);
     }
 
+    private void handleNotify(){
+        notification.createNotification(ID);
+        setButtonState(false, true, true);
+    }
+
+    private void handleUpdate(){
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.test);
+        NotificationCompat.BigPictureStyle style = new NotificationCompat.BigPictureStyle();
+        style.bigPicture(bitmap);
+        style.setBigContentTitle("BIG BIG WORLD");
+        notification.updateNotification(ID, style);
+        tvReply.setText(R.string.tvReply);
+        setButtonState(false, false, true);
+    }
+
+    private void handleCancel(){
+        if (manager != null) manager.cancel(ID);
+        tvReply.setText(R.string.tvReply);
+        setButtonState(true, false, false);
+    }
 
     @OnClick({R.id.bnNotify, R.id.bnUpdate, R.id.bnCancel})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.bnNotify:
+                handleNotify();
                 break;
             case R.id.bnUpdate:
+                handleUpdate();
                 break;
             case R.id.bnCancel:
+                handleCancel();
                 break;
         }
+    }
+
+    @Override
+    public void onBroadcastReceive(CharSequence charSequence) {
+        tvReply.setText(charSequence);
+        if (manager != null) manager.cancel(ID);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(receiver);
     }
 }
--- app/src/main/java/com/haanhgs/broadcastdemo/Notification.java
+++ app/src/main/java/com/haanhgs/broadcastdemo/Notification.java
@@ -1,14 +1,22 @@
 package com.haanhgs.broadcastdemo;
 
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.RemoteInput;
 
 public class Notification {
 
     private static final String CHANNEL_ID = "com.haanhgs.broadcastreceivernotification_ID";
     private static final String CHANNEL_NAME = "com.haanhgs.broadcastreceivernotification";
     private static final int REQUEST = 1979;
-    private static final String REMOTE_ACTION = "action_remote";
+    public static final String REMOTE_ACTION = "action_remote";
     public static final String REMOTE_RESULT = "remote_result";
 
     private final Context context;
@@ -18,4 +26,66 @@ public class Notification {
         this.context = context;
         manager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
     }
+
+    private void createChannel(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && manager != null){
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME,
+                    NotificationManager.IMPORTANCE_HIGH);
+            channel.setDescription("Notice");
+            channel.enableLights(true);
+            channel.enableVibration(true);
+            channel.setLightColor(Color.GREEN);
+            manager.createNotificationChannel(channel);
+        }
+    }
+
+    private PendingIntent createOpenMainIntent(){
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.setAction(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+        return PendingIntent.getActivity(
+                context, REQUEST, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+    private NotificationCompat.Action createRemoteInput(){
+        Intent intent = new Intent(REMOTE_ACTION);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, REQUEST, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+        RemoteInput remoteInput = new RemoteInput.Builder(REMOTE_RESULT).setLabel("Reply").build();
+        return new NotificationCompat.Action.Builder(R.drawable.reply, "Reply", pendingIntent)
+                .addRemoteInput(remoteInput)
+                .build();
+    }
+
+    private NotificationCompat.Builder createBuilder(){
+        return new NotificationCompat
+                .Builder(context, CHANNEL_ID)
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setAutoCancel(true)
+                .setContentIntent(createOpenMainIntent())
+                .setSmallIcon(R.drawable.star)
+                .setContentTitle("Too much")
+                .setContentText("text content");
+    }
+
+    public void createNotification(int id){
+        createChannel();
+        NotificationCompat.Builder builder = createBuilder();
+        builder.addAction(createRemoteInput());
+        if (manager != null){
+            manager.notify(id, builder.build());
+        }
+    }
+
+    public void updateNotification(int id, NotificationCompat.BigPictureStyle style){
+        createChannel();
+        NotificationCompat.Builder builder = createBuilder();
+        builder.setStyle(style);
+        if (manager != null){
+            manager.notify(id, builder.build());
+        }
+    }
+
 }
